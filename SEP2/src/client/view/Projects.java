2162dadcaae0102bdc5c05201de6f11c44917baf
Mogens Bjerregaard <mogens.bjerregaard@mac.com>
2017-05-15 11:32:10 +0200

    Started to work on GUI for projects

--- SEP2/src/client/view/Projects.java
+++ SEP2/src/client/view/Projects.java
@@ -12,6 +12,11 @@ public class Projects extends AbstractJIF {
 	private static final long serialVersionUID = 1L;
 	private JLabel lblProjects;
 	private JTree tree;
+	private JButton btnNewProject;
+	private JButton btnDelProject;
+	private JButton btnAddUser;
+	private JButton btnDelUser;
+	private JList listUsers;
 	
 	public Projects() {
 		setBounds(20, 20, 450, 300);
@@ -49,6 +54,26 @@ public class Projects extends AbstractJIF {
 		tree.setBounds(10, 60, 322, 694);
 		getContentPane().add(tree);
 		
+		btnNewProject = new JButton("New project");
+		btnNewProject.setBounds(342, 62, 110, 23);
+		getContentPane().add(btnNewProject);
+		
+		btnDelProject = new JButton("Delete project");
+		btnDelProject.setBounds(342, 96, 110, 23);
+		getContentPane().add(btnDelProject);
+		
+		btnAddUser = new JButton("Add user");
+		btnAddUser.setBounds(342, 130, 110, 23);
+		getContentPane().add(btnAddUser);
+		
+		btnDelUser = new JButton("Delete user");
+		btnDelUser.setBounds(342, 164, 110, 23);
+		getContentPane().add(btnDelUser);
+		
+		listUsers = new JList();
+		listUsers.setBounds(342, 152, 150, 160);
+		getContentPane().add(listUsers);
+		
 	}
 
 	public void createEvents(){
@@ -58,5 +83,14 @@ public class Projects extends AbstractJIF {
 				Root.sprintFrame.setVisible(true);
 			}
 		});
+
+		btnNewProject.addActionListener(e -> {
+			
+		});
+
+		btnDelProject.addActionListener(e -> {
+			
+		});
+		
 	}
 }
